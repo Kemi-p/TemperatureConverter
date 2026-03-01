208c23b745b2839e57557b135b0b11057051fd28
kemi-p <kpole@dvtsoftware.com>
2026-03-01 21:25:58 +0200

    temperature conversion logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
-/**
- * 
- * Celsius to Fahrenheit:** $F = C \times \frac{9}{5} + 32$
-* Fahrenheit to Celsius:** $C = (F - 32) \times \frac{5}{9}$
- */
-  function convertCelsiusToFarenheit (celsius : number){
-    return (celsius * (9/ 5 )+ 32);
-  }
 
-  function convertFarenheitToCelsius (farenheit : number){
-    return ((farenheit -32) * (9/5))
-  }
-//mana state past example- used the func Input comp which means that
-//youll have the Temperature input comp
-
-
-//temp input will have props:
- 
-/**
- * 
- * <label>
-      {label}
-      {' '}
-      <input
-        value={value}
-        onChange={onChange}
-      />
-    </label>
- */
-// i need to remember 
-//youll need to handle the change for both when they change
-//so handlechange for the celcius change and then handle change for the faren change 
-//how am i gonna 
 function App() {
 
   return (
---
+++ src/utils/temperatureConversion.ts
@@ -0,0 +1,26 @@
+//handling all my convesions using open/closed principle
+import type { TemeperatureUnit } from "../types/temperature";
+
+/**
+ * 
+ * **Celsius to Fahrenheit:** $F = C \times \frac{9}{5} + 32$
+* **Fahrenheit to Celsius:** $C = (F - 32) \times \frac{5}{9}$
+ */
+//my function taking in a num val and 2 temp uits and the returns a number 
+export function Convert(
+    value:number,
+    fromUnit: TemeperatureUnit,
+    toUnit: TemeperatureUnit
+) : number {
+    if (fromUnit === toUnit) return value;
+
+    if (fromUnit === 'celcius' && toUnit === 'farenheit'){
+        return ((value * (9/5)) +32);
+    }
+
+    if (fromUnit === 'farenheit' && toUnit === "celcius"){
+        return ((value - 32) * (5/9));
+    }
+
+    return value;
+}
